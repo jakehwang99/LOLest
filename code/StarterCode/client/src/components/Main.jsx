bb7d7af45f99e5697017b541bcd197c5f834169e
Michael Hsieh <michaelhsieh92@gmail.com>
2019-10-31 12:52:01 -0700

    New Dev Branch

--- code/StarterCode/client/src/components/Main.jsx
+++ code/StarterCode/client/src/components/Main.jsx
@@ -10,27 +10,11 @@ class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
-        // this.getTeams();
-    }
-
-    getTeams = () => {
-        axios.get('http://localhost:5000/teams')
-          .then((response) => {
-            // handle success
-            this.setState({ team: response.data });
-            console.log(response.data);
-        })
-          .catch(function (error) {
-            // handle error
-        console.log(error);
-        })
     }
 
     render () {
-      const { team } = this.state;
       return (
         <div>
-          {/* {team && <div> Retrieved team from backend: {team} </div>} */}
           <MainHeader />
           <Image src={homeBackground} className="w-100 p-0"/>
           <ButtonToolbar className="buttons">
