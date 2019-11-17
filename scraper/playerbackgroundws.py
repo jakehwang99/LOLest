00d2f24634de1714e634090ca5f880a6be1385f2
Gus Person <gperson@ucsc.edu>
2019-11-16 17:42:50 -0800

    removed unneccesary prints, put scraped data into mongodb

--- scraper/playerbackgroundws.py
+++ scraper/playerbackgroundws.py
@@ -1,19 +1,26 @@
 from bs4 import BeautifulSoup
 from requests import get
+from pymongo import MongoClient
+import json
 import pandas as pd
 
-teams = ['LCS', 'LCK', 'LEC', 'LPL']
+
+client = MongoClient('mongodb+srv://billy:test@lolest0-t8qkt.mongodb.net/test?retryWrites=true&w=majority')
+db = client['LOLplayers']
+
+
+
+leagues = ['LCS', 'LCK', 'LEC', 'LPL']
 columns = ['Name', 'Country of Birth', 'Birthday', 'Residency', 'Team', 'Role']
 df = pd.DataFrame(columns=columns)
-for team in teams:
-    url = 'https://lol.gamepedia.com/' + team + '/2019_Season/Spring_Season/Player_Statistics'
+for league in leagues:
+    url = 'https://lol.gamepedia.com/' + league + '/2019_Season/Spring_Season/Player_Statistics'
     response = get(url)
     html_soup = BeautifulSoup(response.text, 'html.parser')
     type(html_soup)
     div = html_soup.find('div', {'class': 'wide-content-scroll'})
     table = div.find('table')
     rows = table.findAll('tbody')[0].findAll('tr')
-    print(df)
     players = []
     for row in rows:
         player = row.findAll('td')
@@ -59,4 +66,7 @@ for team in teams:
         background = {'Name': [name], 'Country of Birth': [bCountry], 'Birthday': [birthday], 'Residency': [residence], 'Team': [team], 'Role': [role]}
         dfRow = pd.DataFrame(background, columns = columns)
         df = pd.concat([df, dfRow], ignore_index=True)
-    print(df)
+    
+    collection = db[league]
+    data = json.loads(df.T.to_json()).values()
+    collection.insert_many(data)
