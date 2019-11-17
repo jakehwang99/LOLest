a0bae09ee669901ec7ebf2b9d022344cd51a8a0a
Michael Hsieh <michaelhsieh92@gmail.com>
2019-11-17 14:14:35 -0800

    Basic setup for individual player pages

--- code/StarterCode/client/src/components/DataPage/Body.jsx
+++ code/StarterCode/client/src/components/DataPage/Body.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Tabs, Tab, TabList, TabPanel } from "react-tabs";
 import Select from 'react-select';
 import Table from './Table.jsx';
+import Cards from './Cards.jsx';
 import './datapagelayout.css';
 import 'react-tabs/style/react-tabs.css';
 
@@ -10,7 +11,7 @@ class Body extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeTab: 1,
+            activeTab: 0,
             selectedOption: null,
         };
     }
@@ -24,7 +25,8 @@ class Body extends React.Component {
 
     render () {
       const { activeTab, selectedOption } = this.state;
-      const { options, leagueData } = this.props;    
+      const { options, leagueData } = this.props;   
+      
       return (
         <div className="body">
             <div className="tabs">
@@ -38,10 +40,14 @@ class Body extends React.Component {
                   <Tab> Table </Tab>
                   <Tab> Line Chart </Tab>
                   <Tab> Bar Graph </Tab>
+                  <Tab> Player Info </Tab>
               </TabList>
               <TabPanel><Table data={leagueData} /></TabPanel>
               <TabPanel>Line Chart</TabPanel>
               <TabPanel>Bar Graph</TabPanel>
+              <TabPanel>
+                 <Cards />
+              </TabPanel>
             </Tabs>
             </div>
         </div>      
---
+++ code/StarterCode/client/src/components/DataPage/Cards.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import Faker from '../../images/sktFaker.png';
+
+class Cards extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render (){
+        return(
+            <div>
+                <Card className="text-center">
+                    <Card.Img className="mx-auto d-block" style={{ width: '18rem' }} variant="top" src={Faker} />
+                    <Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem>
+                                <Card.Title>Name</Card.Title>
+                                <Card.Text>
+                                    Player's name goes here
+                                </Card.Text>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Card.Title>Country of Birth</Card.Title>
+                                <Card.Text>
+                                    Country of Birth goes here
+                                </Card.Text>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Card.Title>Birthday</Card.Title>
+                                <Card.Text>
+                                    Birthday goes here
+                                </Card.Text>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Card.Title>Residency</Card.Title>
+                                <Card.Text>
+                                    Residency goes here
+                                </Card.Text>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Card.Title>Team</Card.Title>
+                                <Card.Text>
+                                    Team goes here
+                                </Card.Text>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Card.Title>Role</Card.Title>
+                                <Card.Text>
+                                    Role goes here
+                                </Card.Text>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card.Body>
+                </Card>  
+            </div>
+        );
+    }
+
+}
+
+export default Cards;
