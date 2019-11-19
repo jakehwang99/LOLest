6ba8946fd01e159601a7c547df3aa6638d12d52c
Perry Yang <byang33@ucsc.edu>
2019-11-18 21:33:24 -0800

    retrieve data from backend

--- code/StarterCode/client/src/components/DataPage/Body.jsx
+++ code/StarterCode/client/src/components/DataPage/Body.jsx
@@ -7,6 +7,8 @@ import Cards from './Cards.jsx';
 import './datapagelayout.css';
 import 'react-tabs/style/react-tabs.css';
 
+const axios = require('axios');
+
 class Body extends React.Component {
     constructor(props) {
         super(props);
@@ -17,9 +19,19 @@ class Body extends React.Component {
     }
 
     handleChange = selectedOption => {
+      const player_name = "Perry"
+      const player_url = `http://localhost:5000/LCK/${player_name}/page`;
+
+      axios.get(player_url)
+          .then((response) => {
+                this.setState({ playerPage: response.data.data });
+          })
+          .catch(function (error) {
+                console.log(error);
+        })
       this.setState(
         { selectedOption },
-        () => console.log(`Option selected:`, this.state.selectedOption)
+        () => console.log(`Option selected:`, this.state.selectedOption, this.state.playerPage)
       );
     };
 
